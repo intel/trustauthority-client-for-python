edc52cfb59e17b8ca0462f74f03371c54a91fa5b
Eunjung Yoon <eunjung.yoon@intel.com>
2024-05-29 04:26:38 +0000

    Updated with attest command that takes request id as an option (-r) and added policy UUID validation

--- inteltrustauthorityclient/cli/trustauthority-pycli/trustauthority-pycli.py
+++ inteltrustauthorityclient/cli/trustauthority-pycli/trustauthority-pycli.py
@@ -2,50 +2,69 @@
 
 import argparse
 import json
+import uuid
 import sys
 import os
 import base64
 import logging as log
 from urllib.parse import urlparse
 from dotenv import load_dotenv
-from inteltrustauthorityclient.resources import constants as const
+from inteltrustauthorityclient.resources import constants as constants
 from inteltrustauthorityclient.connector import config, connector
 from inteltrustauthorityclient.tdx.intel.tdx_adapter import TDXAdapter
 from inteltrustauthorityclient.nvgpu.gpu_adapter import GPUAdapter
 
-log.disable(log.NOTSET)
-
 def cmd_evidence(args):
-
     if args.attest_type == 'tdx':
         if args.user_data:
-           #    user_data_bytes = base64.b64decode(args.user_data)
             user_data_bytes = args.user_data
-        else:
-            #user_data = "data generated inside tee"
-            user_data_bytes = ""
 
         if args.nonce:
             nonce_bytes = base64.b64decode(args.nonce)
-        else:
-            nonce_bytes = None
 
+        # eventLogger is not used for Python CLI
         tdx_adapter = TDXAdapter(user_data_bytes, None)
-        #tdx_adapter = TDXAdapter(args.user_data)
         evidence = tdx_adapter.collect_evidence(nonce_bytes)
         if evidence == None:
            log.error("Failed to get the evidence") 
-        print(evidence.quote)
-        #log.info("Quote : %s", base64.b64encode(evidence.quote).decode())
+        log.info(f"TDX quote : {evidence.quote}")
 
     elif args.attest_type == 'nvgpu':
         gpu_adapter = GPUAdapter()
 
         evidence = gpu_adapter.collect_evidence(args.nonce)
-        print(evidence.evidence)
+        log.info(f"GPU evidence : {evidence.evidence}")
 
 def cmd_attest(args):
+    # Check if request id is valid
+    if args.request_id != None:
+       if len(args.request_id) > constants.REQUEST_ID_MAX_LEN:
+           log.error("Request ID should be atmost 128 characters long.") 
+           exit(1)
+       for req_char in args.request_id:
+           if req_char != '-' and req_char.isalnum() == False:
+               log.error("Request ID should contain only a-z, A-Z, 0-9, and - (hyphen), Special characters are not allowed.")
+               exit(1)
 
+       trust_authority_request_id = args.request_id
+
+    # Check if policy uuid is valid and the number of policy counts not exceeded.
+    policyIds = []
+    if args.policy_ids != None:
+       pIds = args.policy_ids.split(",")
+       if len(pIds) > constants.POLICY_IDS_MAX_LEN:
+           log.error("policy count in request must be between 1 - 10")
+           exit(1)
+       for pId in pIds:
+           try:
+               uid = uuid.UUID(pId)
+           except ValueError:
+               log.error(f"Invalid policy UUID :{pId}")
+               exit(1)
+           else:
+               policyIds.append(uid)
+
+    # Read ITA Env configuration from json file
     with open(args.config, 'r') as cf:
         cf_dict = json.load(cf)
 
@@ -62,16 +81,6 @@ def cmd_attest(args):
     trustauthority_api_key = cf_dict['trustauthority_api_key']
     if trustauthority_api_key is None:
         log.error("TRUSTAUTHORITY_API_KEY is not set.")
-        exit(1)
-
-    trust_authority_request_id = cf_dict['trust_authority_request_id']
-    if trust_authority_request_id is None:
-        log.error("TRUST_AUTHORITY_REQUEST_ID is not set.")
-        exit(1)
-
-    trust_authority_policy_id = cf_dict['trust_authority_policy_id']
-    if trust_authority_policy_id is None:
-        log.error("TRUST_AUTHORITY_POLICY_ID is not set.")
         exit(1)
 
     cf.close()
@@ -93,21 +102,17 @@ def cmd_attest(args):
 
     if args.attest_type == 'tdx':
         if args.user_data:
-            #user_data_bytes = base64.b64decode(args.user_data)
             user_data_bytes = args.user_data
         else:
-            #user_data = "data generated inside tee"
             user_data_bytes = "" 
         tdx_adapter = TDXAdapter(user_data_bytes, None)
-        tdx_attest_args = connector.TDXAttestArgs(tdx_adapter, trust_authority_request_id)
-        #attestation_token = ita_connector.attest_tdx(attest_args)
+        tdx_attest_args = connector.TDXAttestArgs(tdx_adapter, trust_authority_request_id, policyIds)
         attestation_token = ita_connector.attest_composite(tdx_attest_args, None)
         if attestation_token is None:
             log.error("Attestation Token is not returned.")
             exit(1)
 
         token = attestation_token.token
-        #print(token)
         log.info(f"Attestation token : {token}")
         token_headers_json = json.loads(attestation_token.headers.replace("'", '"'))
         log.info(
@@ -134,28 +139,15 @@ def cmd_attest(args):
 
     elif args.attest_type =='tdx+nvgpu':
         if args.user_data:
-            #user_data_bytes = base64.b64decode(args.user_data)
             user_data_bytes = args.user_data
-        else:
-            #user_data = "data generated inside tee"
-            user_data_bytes = ""
-
-        if args.policy_ids != None:
-            # policy_ids: An array of one to ten attestation policy IDs.
-            if len(args.policy_ids) > constants.POLICY_IDS_MAX_LEN:
-                log.error("policy count in request must be between 1 - 10")
-                return None
-
-            policy_ids = args.policy_ids
-        else:
-            policy_ids = None
 
         # Create TDX Adapter
         tdx_adapter = TDXAdapter(user_data_bytes, None)
         # Create GPU Adapter
         gpu_adapter = GPUAdapter()
 
-        tdx_attest_args = connector.TDXAttestArgs(tdx_adapter, trust_authority_request_id, policy_ids)
+        tdx_attest_args = connector.TDXAttestArgs(tdx_adapter, trust_authority_request_id, policyIds)
+        # GPU appraisal policy is not supported yet until v2 policy is available. 
         gpu_attest_args = connector.GPUAttestArgs(gpu_adapter, trust_authority_request_id, None)
         # Fetch Attestation Token from ITA
         attestation_token = ita_connector.attest_composite(tdx_attest_args, gpu_attest_args)
@@ -163,7 +155,7 @@ def cmd_attest(args):
             log.error("Attestation Token is not returned.")
             exit(1)
         token = attestation_token.token
-        #log.info(f"Attestation token : {token}")
+        log.info(f"Attestation token : {token}")
         token_headers_json = json.loads(attestation_token.headers.replace("'", '"'))
         log.info(
             "Request id and Trace id are: %s, %s",
@@ -223,14 +215,11 @@ def cmd_verify(args):
             verified_token = None
             log.error(f"Token verification returned exception : {exc}")
         if verified_token != None:
-            print("Token Verification Successful")
-            print(f"Verified Attestation Token : {verified_token}")
             log.info("Token Verification Successful")
             log.info(f"Verified Attestation Token : {verified_token}")
         else:
             log.error("Token Verification failed")
             exit(1)
-
 
 def main():
     parser = argparse.ArgumentParser(description='Trust Authority CLI for Python')
@@ -249,6 +238,7 @@ def main():
     parser_attest.add_argument('-c', '--config', type=str, required=True, help='ITA environment argument')
     parser_attest.add_argument('-u', '--user-data', help='User Data in base64 encoded format')
     parser_attest.add_argument('-p', '--policy-ids', help='Trust Authority Policy Ids, comma separated')
+    parser_attest.add_argument('-r', '--request-id', help='Trust Authority Request Id')
     parser_attest.set_defaults(func=cmd_attest)
 
     # verify command
